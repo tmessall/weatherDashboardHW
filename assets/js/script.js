9ed4b57f8c113252e52f298d947baeeb7077a47e
tmessall <tmessall17@gmail.com>
2020-10-06 20:41:55 -0600

    Added functionality to store all cities

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,24 @@
 $(document).ready(function() {
     var cityInput = $(".city-input");
     var subBtn = $(".city-submit");
+    var numCities = 0;
+    var citiesArr = [];
+    var newCity = true;
+    while (newCity) {
+        var makeCity = localStorage.getItem(`cityNo${numCities}`);
+        if (makeCity == null) {
+            newCity = false;
+        } else {
+            citiesArr.push(makeCity);
+            numCities++;
+        }
+    }
+    console.log(citiesArr);
 
+    // This saves the city to search history
     subBtn.click(function(event) {
         event.preventDefault();
-        localStorage.setItem("city", cityInput.val());
+        localStorage.setItem(`cityNo${numCities}`, cityInput.val());
+        numCities++;
     })
 })
