74f7823a4da95b33a561c624238d8c9914aa0abe
tmessall <tmessall17@gmail.com>
2020-10-06 21:42:29 -0600

    Added ability to show the city's name and current temperature

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
     var cityInput = $(".city-input");
     var subBtn = $(".city-submit");
     var cityList = $(".show-cities");
+    var curWeather = $(".current-weather");
     // Makes the list of cities to print out from local storage
     var numCities = 0;
     var citiesArr = [];
@@ -29,8 +30,8 @@ $(document).ready(function() {
             cityList.append(liTag);
         }
     }
-    displayCities();
 
+    // Displays the weather
     function displayWeather(cityDisplaying) {
 
         // My key is "d9131680735a7b4b06edce9e340f8e49" for when it works
@@ -42,8 +43,28 @@ $(document).ready(function() {
             method: "GET"
           }).then(function (response) {
             console.log(response);
+            // Gets date using the UNIX timestamp
+            var date = new Date((response.sys.sunrise * 1000));
+            // The name it'll show (which includes date)
+            var showName = cityDisplaying + " " + date.toLocaleDateString();
+            // Displays the name
+            var name = $("<h1>");
+            name.text(showName);
+            curWeather.append(name);
+
+            // Gets temperature, converts to fahrenheit
+            var tempKelv = response.main.temp;
+            var tempFar = Math.floor((tempKelv - 273.15) * 9 / 5 + 32);
+            // Displays temperature
+            var temp = $("<p>");
+            temp.text("Temperature: " + tempFar + " F");
+            curWeather.append(temp);
           })
     }
+
+    // Displays all cities at first and a default weather area
+    displayCities();
+    displayWeather(citiesArr[0]);
 
     // This saves the city to search history
     subBtn.click(function(event) {
@@ -59,6 +80,7 @@ $(document).ready(function() {
         }
     });
 
+    // Displays this cities weather when its button is clicked
     $(".city-button").click(function(event) {
         displayWeather($(this).text());
     });
