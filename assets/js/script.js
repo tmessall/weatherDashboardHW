283a46486aaa8aec9a4117a2f9501faeca0fffb6
tmessall <tmessall17@gmail.com>
2020-10-06 20:48:17 -0600

    Added functionality to see all cities

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
     var cityInput = $(".city-input");
     var subBtn = $(".city-submit");
+    var cityList = $(".show-cities");
+    // Makes the list of cities to print out from local storage
     var numCities = 0;
     var citiesArr = [];
     var newCity = true;
@@ -13,12 +15,25 @@ $(document).ready(function() {
             numCities++;
         }
     }
-    console.log(citiesArr);
+
+    // Displays all the cities
+    function displayCities() {
+        cityList.empty();
+        for (var i = 0; i < citiesArr.length; i++) {
+            var liTag = $("<li>");
+            liTag.text(citiesArr[i]);
+            cityList.append(liTag);
+        }
+    }
+
+    displayCities();
 
     // This saves the city to search history
     subBtn.click(function(event) {
         event.preventDefault();
         localStorage.setItem(`cityNo${numCities}`, cityInput.val());
+        citiesArr.push(cityInput.val());
         numCities++;
+        displayCities();
     })
 })
